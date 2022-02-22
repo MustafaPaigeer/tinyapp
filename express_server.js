6b3afc1f74f84ac190b90becfccf284a04343d2c
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-22 07:05:35 +0000

    add bodyParser to server

--- express_server.js
+++ express_server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xk": "http://www.google.com"
 };
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
@@ -18,6 +24,7 @@ app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
     res.render("urls_show", templateVars);
 });
+
 
 app.get("/", (req, res) => {
     res.send("Hello!");
