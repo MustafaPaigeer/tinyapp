74e9381a66ba1f301ae2472bc26f74fd46ffea41
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-22 07:41:14 +0000

    Add random string generator function and new res path to redirect to short URL

--- express_server.js
+++ express_server.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6);
+}
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xk": "http://www.google.com"
@@ -19,12 +22,20 @@ app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
     res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+    const longURL = generateRandomString();
+    res.redirect(longURL);
+  });
 
 app.get("/", (req, res) => {
     res.send("Hello!");
