d62985a4c70ecb475e41754ee99259dad883696f
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-27 06:58:50 +0000

    Code refactoring

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -1,16 +1,16 @@
-const res = require("express/lib/response");
+//const res = require("express/lib/response");
 const bcrypt = require("bcryptjs");
 
-
-function generateRandomString() {
+// Create random userID and shortURL
+const generateRandomString = () => {
   return Math.random().toString(20).substr(2, 6);
-}
+};
 
-const createUser = function(userDB, user) {
+const createUser = (userDB, user) => {
   const { email, password } = user;
   const userData = Object.values(userDB);
   if (email === '' || password === '') {
-    return {error: "Email or password can't be empty", data: null}
+    return {error: "Email or password can't be empty", data: null};
   }
   for (const user of userData) {
     if (user.email === email) {
@@ -19,69 +19,65 @@ const createUser = function(userDB, user) {
   }
   const uid = generateRandomString();
   const hashedPassword = bcrypt.hashSync(password, 10);
-    userDB[uid] = {id: uid, email: email, password: hashedPassword};
-    console.log(userDB[uid])
-    return {error: null, data: userDB[uid]}
+  userDB[uid] = {id: uid, email: email, password: hashedPassword};
+  return {error: null, data: userDB[uid]};
 };
 
-const createNewUrl = function(urlDatabase, longUrl, user_Id) {
-  console.log(longUrl)
+const createNewUrl = (urlDatabase, longUrl, userId) => {
   if (longUrl === '') {
-    return {error: "URL cannot be empty", data: null}
+    return {error: "URL cannot be empty", data: null};
   } else {
     const shortURL = generateRandomString();
-    urlDatabase[shortURL] = {longURL: longUrl, userID: user_Id}
+    urlDatabase[shortURL] = {longURL: longUrl, userID: userId};
     return { error: null, data: urlDatabase[shortURL] };
   }
 };
 
-const updateUrl = function(urlDatabase, shortUrl, longUrl, user_Id) {
- 
+const updateUrl = (urlDatabase, shortUrl, longUrl, userId) => {
   if (longUrl === '') {
-    return {error: "URL cannot be empty", data: null}
+    return {error: "URL cannot be empty", data: null};
   }
-    if (urlDatabase[shortUrl].userID === user_Id) {
-        urlDatabase[shortUrl] = {longURL: longUrl, userID: user_Id}
-    } else if (urlDatabase[shortUrl].userID !== user_Id) {
-        console.log("UserID doesn't match")
-    }
+  if (urlDatabase[shortUrl].userID === userId) {
+    urlDatabase[shortUrl] = {longURL: longUrl, userID: userId};
+  } else if (urlDatabase[shortUrl].userID !== userId) {
+    console.log("UserID doesn't match");
+  }
   return { error: null, data: urlDatabase[shortUrl]};
 };
 
 const authenticateUser = (userDB, email, password) => {
   const userData = Object.values(userDB);
   if (email === '' || password === '') {
-      return {error: "Email or Password cannot be empty string"}
+    return {error: "Email or Password cannot be empty string"};
   }
   for (const user of userData) {
-    console.log("user: ", password)
-    console.log(user.password)
-      if (user.email === email) {
-        if (bcrypt.hashSync(password, user.password)){
-          return { error: null, data: user };
-        }
+    if (user.email === email) {
+      if (bcrypt.hashSync(password, user.password)) {
+        return { error: null, data: user };
       }
+    }
   }
-  console.log(userDB)
-  return { error: "Email or Password not found", data: null }; 
+  return { error: "Email or Password not found", data: null };
 };
   
 const fetchUserInformation = (userDB, userId) => {
   let userInfo = undefined;
-      if (userDB[userId]) {
-        userInfo = userDB[userId];
-      } else {
-        userInfo = {};
-      }
-     return userInfo;
+  if (userDB[userId]) {
+    userInfo = userDB[userId];
+  } else {
+    userInfo = {};
+  }
+  return userInfo;
 };
+
 const isLoggedIn = (loginToken) => {
   if (Object.keys(loginToken).length === 0) {
-      return false;
-    } else {
-      return true;
-    }
+    return false;
+  } else {
+    return true;
+  }
 };
+
 const filterUrlDb = (urlDatabase, userId) => {
   let urlDB = {};
   for (const url in urlDatabase) {
@@ -89,7 +85,7 @@ const filterUrlDb = (urlDatabase, userId) => {
       urlDB[url] = urlDatabase[url];
     }
   }
-    return urlDB;
+  return urlDB;
 };
 
 module.exports = { filterUrlDb, updateUrl, createNewUrl, fetchUserInformation, authenticateUser, createUser, isLoggedIn };
